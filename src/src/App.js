64dc5df0274a84cbc131cf53b2c80bd29869ec44
jerome2.jouanny <jerome2.jouanny@epitech.eu>
2018-11-07 12:14:21 +0100

    the api is correctly requested

--- src/src/App.js
+++ src/src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import awsmobile from './aws-exports';
+import Amplify,{API} from 'aws-amplify';
 class App extends Component {
-  render() {
+    componentDidMount() {
+        Amplify.configure(awsmobile);
+        this.fetch().then();
+    }
+
+    fetch = async () => {
+        this.setState(() => {
+            return {
+                loading: true
+            }
+        });
+
+        API.get('todolist','/test')
+            .then(resp => {
+                this.setState({
+                    data: resp
+                });
+                console.log("response is : ", resp);
+            })
+            .catch (err => console.log(err))
+    }
+
+
+render() {
     return (
       <div className="App">
         <header className="App-header">
