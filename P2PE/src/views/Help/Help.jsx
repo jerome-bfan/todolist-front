e1c4359e7205373a541f11325776191873167ee5
Guillaume Rodier <guillaume.rodier@epitech.eu>
2019-01-14 14:21:24 +0100

    Section contact (form + infos + map) - Move later to another page

--- P2PE/src/views/Help/Help.jsx
+++ P2PE/src/views/Help/Help.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import { Grid, Row, Col, Collapse, Well, Panel, PanelGroup } from "react-bootstrap";
+import { Grid, Row, Col, Collapse, Well, Panel, PanelGroup,
+          ListGroup, ListGroupItem,
+
+          Form, FormGroup, Label, FormControl, textarea
+        } from "react-bootstrap";
 import {
   getNotes,
   getAllNotes,
@@ -14,11 +18,32 @@ import Card from "components/Card/Card.jsx";
 const style = {
   title : {
     textAlign: 'center',
-    margin: '0 0 30px'
+    margin: '20px 0 30px'
   },
   question: {
     width: '100%',
     textAlign: 'center',
+  },
+  map: {
+    border: '0'
+  },
+  input_style: {
+    paddingLeft: '6px'
+  },
+  submitBtn: {
+    padding: '8px 70px',
+    margin: 'auto',
+    display: 'block',
+    color: '#000000',
+    fontSize: '17px',
+  },
+  line: {
+    display: 'block',
+    height: '1px',
+    border: '0',
+    borderTop: '1px solid #c2c2c2',
+    margin: '2em 0',
+    padding: '0',
   }
 };
 
@@ -114,37 +139,37 @@ class Typography extends Component {
     );
   }
 
-  _renderGoogleCard() {
+  /* Google Map */
+  _renderCard() {
     return (
-      <Col md={12}>
+      <Col md={6}>
+        <div style={{ flexDirection: "column" }}>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2627.2302815922812!2d2.360619315774665!3d48.81566751166777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6717ff972ae09%3A0x692326b123aa4d9b!2sEpitech+Paris!5e0!3m2!1sfr!2sfr!4v1547464720773"
+                  width="100%"
+                  height="450"
+                  frameBorder="0"
+                  style={style.map}
+                  allowFullScreen></iframe>
+        </div>
+      </Col>
+    );
+  }
+
+  _renderContactInfo() {
+    return (
+      <Col md={6}>
         <Card
-          //title="Inscrivez-vous"
-          //category="Remplir les informations ci dessous"
           content={
             <div style={{ flexDirection: "column" }}>
               <div>
                 <div className="App-header">
-                  <h2 style={style.title}>Questions courantes</h2>
+                  <h2 style={style.title}>Comment nous contacter ?</h2>
                 </div>
-                <PanelGroup
-                  accordion
-                  id="accordion_questions"
-                  activeKey={this.state.activeKey}
-                  onSelect={this.handleSelect}
-                >
-                  <Panel eventKey="1">
-                    <Panel.Heading>
-                      <Panel.Title toggle style={style.question}>Panel heading 1</Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body collapsible>Panel content 1</Panel.Body>
-                  </Panel>
-                  <Panel eventKey="2">
-                    <Panel.Heading>
-                      <Panel.Title toggle style={style.question}>Panel heading 2</Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body collapsible>Panel content 2</Panel.Body>
-                  </Panel>
-                </PanelGroup>
+                <ListGroup>
+                  <ListGroupItem><strong>Adresse :</strong> 24, rue pasteur, 94270 Le Kremlin-Bicêtre</ListGroupItem>
+                  <ListGroupItem><strong>Numéro de téléphone :</strong> 06 66 66 66 66</ListGroupItem>
+                  <ListGroupItem><strong>Adresse email :</strong> contact@p2pe.com</ListGroupItem>
+                </ListGroup>
               </div>
             </div>
           }
@@ -152,6 +177,70 @@ class Typography extends Component {
       </Col>
     );
   }
+
+  _renderContactForm() {
+    return (
+      <Form className="form">
+        <Col md={6} style={style.input_style}>
+          <FormGroup>
+            <Label>Nom</Label>
+            <FormControl
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Votre nom"
+            />
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Label htmlFor="examplePassword">Email</Label>
+            <FormControl
+              type="email"
+              name="email"
+              id="customerEmail"
+              placeholder="Votre email"
+            />
+          </FormGroup>
+        </Col>
+        <Col md={12}>
+          <FormGroup controlId="formControlsTextarea">
+            <Label htmlFor="message">Message</Label>
+            <FormControl componentClass="textarea" placeholder="Votre message" />
+          </FormGroup>
+        </Col>
+        <Button style={style.submitBtn}>Envoyer</Button>
+      </Form>
+    );
+  }
+
+  _renderContact() {
+    return (
+      <Col md={12}>
+        <Card
+          content={
+            <Grid fluid>
+              <Row>
+                <div className="App-header">
+                  <h2 style={style.title}>Nous contacter</h2>
+                </div>
+                {this._renderCard()}
+                {this._renderContactInfo()}
+              </Row>
+              <div style={style.line}></div>
+              <Row>
+                <div className="App-header">
+                  <h2 style={style.title}>Nous contacter</h2>
+                </div>
+                {this._renderContactForm()}
+              </Row>
+            </Grid>
+          }
+        />
+      </Col>
+    );
+  }
+  /**/
 
   deleteNote() {
     deleteNoteAdmin(this.id_note.value).then();
@@ -169,7 +258,7 @@ class Typography extends Component {
           </Row>
           <Row>
             <h2 style={style.title}>Page contact</h2>
-            {this._renderGoogleCard()}
+            {this._renderContact()}
           </Row>
         </Grid>
       </div>
