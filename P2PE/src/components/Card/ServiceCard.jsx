2b906712133df33323ed9c9c6c2cfc4d4a17d9c2
jerome2.jouanny <jerome2.jouanny@epitech.eu>
2019-01-27 22:50:57 +0100

    move price and valdiate button for a bettter design

--- P2PE/src/components/Card/ServiceCard.jsx
+++ P2PE/src/components/Card/ServiceCard.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
 import { Card2 } from "components/Card/Card";
+import Button from "components/CustomButton/CustomButton.jsx";
 
 export class ServiceCard extends Component {
+  _renderButtonValidate = () => {
+    return (
+      <Button style={{ marginTop: 15 }} onClick={e => {}}>
+        RESERVEZ
+      </Button>
+    );
+  };
   _renderContent() {
     return (
       <div key={this.props.id}>
         {" "}
         <Card2
           title={this.props.title}
-          category={<div>{this.props.categoryName}   ({this.props.location})</div>}
-          bottomRight={this.props.categoryName}
-          topRight={this.props.prix}
+          category={
+            <div>
+              {this.props.categoryName} ({this.props.location})
+            </div>
+          }
+          bottomRight={<div>{this._renderButtonValidate()}</div>}
+          topRight={
+            <div style={{ marginTop: 6 ,fontWeight: "bold", fontSize: 40 }}>
+              {this.props.prix}€
+            </div>
+          }
           content={<div>{this.props.description}</div>}
         />{" "}
       </div>
