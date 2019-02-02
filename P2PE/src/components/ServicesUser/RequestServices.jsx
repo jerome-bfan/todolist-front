9dc6b0d5a2e55f8bf37e7825e10c3cd59173600d
jerome2.jouanny <jerome2.jouanny@epitech.eu>
2019-02-02 12:27:55 +0100

    implement modal to  requestservice component

--- P2PE/src/components/ServicesUser/RequestServices.jsx
+++ P2PE/src/components/ServicesUser/RequestServices.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import { getServiceUser } from "../../Provider/Api";
 import Card from "components/Card/Card";
-import { Grid, Row, Col, Panel, PanelGroup } from "react-bootstrap";
+import { colorRole } from "../../functions/p2peFunction";
+import { Grid, Row, FormInputs,Col, Panel, PanelGroup,Modal } from "react-bootstrap";
+import Button from "components/CustomButton/CustomButton.jsx";
 
 import { iconsArray } from "variables/Variables.jsx";
 import { style } from "variables/Variables.jsx";
+import { FormInputs } from 'components/FormInputs/FormInputs.jsx';
 
 class RequestServices extends Component {
   constructor(props) {
     super(props);
     this.renderContent = this.renderContent.bind(this);
-
+    this.renderHeader = this.renderHeader.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleValidate = this.handleValidate.bind(this);
     this.state = {
       services: []
     };
@@ -19,11 +25,25 @@ class RequestServices extends Component {
     getServiceUser().then(services => {
       console.log(services);
       this.setState({
+        show: false,
         services: services.data
       });
     });
   }
+  handleClose() {
+    this.setState({ show: false });
+  }
 
+  handleValidate() {
+    // console.log(this.props.id)
+    //  postServiceUser(this.props.id,this.state.address).then(()=> {
+    //   this.setState({ show: false });
+    //  });
+    }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
   renderContent(service) {
     if (service.paid) {
       return (
@@ -38,6 +58,46 @@ class RequestServices extends Component {
             <text>
               Vous devez payer votre service valider car le pro la validé
             </text>
+            <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Validez le service</Modal.Title>
+          </Modal.Header>
+          <Modal.Body> <FormInputs
+                        ncols={["col-md-12"]}
+                        proprieties={[
+                          {
+                            label: "Veuillez rentrer votre adresse",
+                            type: "text",
+                            id: "address",
+                            bsClass: "form-control",
+                            placeholder: "Saisir votre addresse ici",
+                            onChange: this.handleChange,
+                            value: this.state.address
+                          }
+                        ]}
+                      /></Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            
+            <Button variant="primary" onClick={this.handleValidate}>
+             Validez votre adresse !
+            </Button>
+          </Modal.Footer>
+        </Modal>
+            <Button
+                        style={{
+                          marginLeft:30,
+                          borderColor: colorRole("#888888"),
+                          color: colorRole("#888888")
+                        }}
+                        onClick={e => {
+                          this.addService();
+                        }}
+                      >
+                        Vous devez valider votre service ! 
+                      </Button>
           </div>
         );
       } else if (!service.validated) {
@@ -46,6 +106,34 @@ class RequestServices extends Component {
             <text>
               Vous n'avez pas encore payer car le pro n'as pas encore validé sa
               tâche
+            </text>
+          </div>
+        );
+      }
+    }
+  }
+  renderHeader(service) {
+    if (service.paid) {
+      return (
+        <div>
+          <text>Service payé :</text>
+        </div>
+      );
+    } else if (!service.paid) {
+      if (service.validated) {
+        return (
+          <div>
+            <text>
+              En attente de payement :
+            </text>
+        
+          </div>
+        );
+      } else if (!service.validated) {
+        return (
+          <div>
+            <text>
+              En attente de validation :
             </text>
           </div>
         );
@@ -70,7 +158,7 @@ class RequestServices extends Component {
                       <div className="">
                         <div className="App-header">
                           <h2 style={style.title}>
-                            Adresse : {service.address}
+                          {this.renderHeader(service)}
                           </h2>
                         </div>
                         <PanelGroup
