423cd6e888c2cfc1631dfa4e95290a66932b50bb
jerome2.jouanny <jerome2.jouanny@epitech.eu>
2019-02-02 12:12:08 +0100

    render content of requestservice component

--- P2PE/src/components/ServicesUser/RequestServices.jsx
+++ P2PE/src/components/ServicesUser/RequestServices.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import { getServiceUser } from "../../Provider/Api";
 import Card from "components/Card/Card";
-import { Grid, Row, Col, Panel, PanelGroup, } from "react-bootstrap";
+import { Grid, Row, Col, Panel, PanelGroup } from "react-bootstrap";
 
 import { iconsArray } from "variables/Variables.jsx";
 import { style } from "variables/Variables.jsx";
 
-
 class RequestServices extends Component {
   constructor(props) {
     super(props);
+    this.renderContent = this.renderContent.bind(this);
+
     this.state = {
       services: []
     };
- 
   }
   componentWillMount() {
     getServiceUser().then(services => {
@@ -23,47 +23,80 @@ class RequestServices extends Component {
       });
     });
   }
+
+  renderContent(service) {
+    if (service.paid) {
+      return (
+        <div>
+          <text>Vous avez payer le service !</text>
+        </div>
+      );
+    } else if (!service.paid) {
+      if (service.validated) {
+        return (
+          <div>
+            <text>
+              Vous devez payer votre service valider car le pro la validé
+            </text>
+          </div>
+        );
+      } else if (!service.validated) {
+        return (
+          <div>
+            <text>
+              Vous n'avez pas encore payer car le pro n'as pas encore validé sa
+              tâche
+            </text>
+          </div>
+        );
+      }
+    }
+  }
+
   render() {
     console.log(this.state.services);
 
     return (
       <div className="radio">
-        {
+        {(this.state.services != undefined && this.state.services.length) > 0 &&
           this.state.services.map(service => {
-              return          <Col md={12}>
-        <Card
-          //title="Inscrivez-vous"
-          //category="Remplir les informations ci dessous"
-          content={
-            <div style={{ flexDirection: "column" }}>
-              <div className="">
-                <div className="App-header">
-                  <h2 style={style.title}>Adresse : {service.address}</h2>
-                </div>
-                <PanelGroup
-                  accordion
-                  id="accordion_problems"
-                  activeKey={service.date}
-                  onSelect={this.handleSelect}
-                >
-                  <Panel eventKey="1">
-                    <Panel.Heading>
-                      <Panel.Title toggle style={style.question}>{service.date}</Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body>
-                      Notre site vous propose de vous mettre en relation avec
-                      des pros. Ces derniers vous proposeront de multiples
-                      services, que ce soit pour des déménagements, du jardinage
-                      ou tout autre action du quotidien.
-                    </Panel.Body>
-                  </Panel>
-                </PanelGroup>
-              </div>
-            </div>
-          }
-        />
-      </Col>
-            })}
+            return (
+              <Col md={12}>
+                <Card
+                  //title="Inscrivez-vous"
+                  //category="Remplir les informations ci dessous"
+                  content={
+                    <div style={{ flexDirection: "column" }}>
+                      <div className="">
+                        <div className="App-header">
+                          <h2 style={style.title}>
+                            Adresse : {service.address}
+                          </h2>
+                        </div>
+                        <PanelGroup
+                          accordion
+                          id="accordion_problems"
+                          activeKey={service.date}
+                          onSelect={this.handleSelect}
+                        >
+                          <Panel eventKey="1">
+                            <Panel.Heading>
+                              <Panel.Title toggle style={style.question}>
+                                {service.date}
+                              </Panel.Title>
+                            </Panel.Heading>
+                            <Panel.Body>
+                              {this.renderContent(service)}
+                            </Panel.Body>
+                          </Panel>
+                        </PanelGroup>
+                      </div>
+                    </div>
+                  }
+                />
+              </Col>
+            );
+          })}
       </div>
     );
   }
