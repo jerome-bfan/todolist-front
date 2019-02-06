fc5d40b5f0c067fbd62504a18510aea6d23670d9
jerome2.jouanny <jerome2.jouanny@epitech.eu>
2019-02-07 00:03:43 +0100

    git cherry pick commiot

--- P2PE/src/Provider/Api.js
+++ P2PE/src/Provider/Api.js
@@ -307,7 +307,7 @@ export function putUpdateServicePro(service,id) {
     )
     .then(function(result) {
       console.log(result);
-
+      window.location.reload()
       return result;
     })
     .catch(function(error) {
