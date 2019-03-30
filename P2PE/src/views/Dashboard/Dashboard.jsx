f441bd8f1dd40acfd08c731074cdb848264b4a07
tos9lm <guillaume.rodier@epitech.eu>
2019-03-30 18:24:59 +0100

    google map marker

--- P2PE/src/views/Dashboard/Dashboard.jsx
+++ P2PE/src/views/Dashboard/Dashboard.jsx
@@ -14,7 +14,7 @@ import AWS from "aws-sdk/dist/aws-sdk-react-native";
 import { Card } from "components/Card/Card.jsx";
 import NotificationSystem from "react-notification-system";
 
-import GoogleApiWrapper from "../Maps/GGMap";
+import Map from "../Maps/GGMap";
 
 import {
   authentification,
@@ -237,41 +237,46 @@ class Dashboard extends Component {
 
   _rendrePresentation() {
     return (
-      <Col md={12}>
-        <Card
-          //title="Inscrivez-vous"
-          //category="Remplir les informations ci dessous"
-          content={
-            <div style={{ flexDirection: "column" }}>
-              <div className="">
-                <div className="App-header">
-                  <h2 style={style.title}>Présentation de l'entreprise</h2>
+      <div>
+        <Col md={12} style={{ paddingLeft: "5px", paddingRight: "5px", height: "600px" }}>
+          <Map></Map>
+        </Col>
+        <Col md={12}>
+          <Card
+            //title="Inscrivez-vous"
+            //category="Remplir les informations ci dessous"
+            content={
+              <div style={{ flexDirection: "column" }}>
+                <div className="">
+                  <div className="App-header">
+                    <h2 style={style.title}>Présentation de l'entreprise</h2>
+                  </div>
+                  <PanelGroup
+                    accordion
+                    id="accordion_problems"
+                    activeKey={this.state.activeKey}
+                    onSelect={this.handleSelect}
+                  >
+                    <Panel eventKey="1">
+                      <Panel.Heading>
+                        <Panel.Title toggle style={style.question}>
+                          Ce que nous faisons
+                        </Panel.Title>
+                      </Panel.Heading>
+                      <Panel.Body>
+                        Notre site vous propose de vous mettre en relation avec
+                        des pros. Ces derniers vous proposeront de multiples
+                        services, que ce soit pour des déménagements, du jardinage
+                        ou tout autre action du quotidien.
+                      </Panel.Body>
+                    </Panel>
+                  </PanelGroup>
                 </div>
-                <PanelGroup
-                  accordion
-                  id="accordion_problems"
-                  activeKey={this.state.activeKey}
-                  onSelect={this.handleSelect}
-                >
-                  <Panel eventKey="1">
-                    <Panel.Heading>
-                      <Panel.Title toggle style={style.question}>
-                        Ce que nous faisons
-                      </Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body>
-                      Notre site vous propose de vous mettre en relation avec
-                      des pros. Ces derniers vous proposeront de multiples
-                      services, que ce soit pour des déménagements, du jardinage
-                      ou tout autre action du quotidien.
-                    </Panel.Body>
-                  </Panel>
-                </PanelGroup>
               </div>
-            </div>
-          }
-        />
-      </Col>
+            }
+          />
+        </Col>
+      </div>
     );
   }
 
--- P2PE/src/views/Maps/GGMap.jsx
+++ P2PE/src/views/Maps/GGMap.jsx
@@ -1,24 +1,87 @@
 import React, { Component } from "react";
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-export class MapContainer extends Component {
-  render() {
-    return (
-      <Map google={this.props.google} zoom={14}>
+const style = {
+  width: '100%',
+  height: '100%'
+}
 
-        <Marker onClick={this.onMarkerClick}
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+
+  render() {
+    var points = [
+        { lat: 48.814819, lng: 2.373283 },
+        { lat: 48.815619, lng: 2.373283 },
+        { lat: 48.816419, lng: 2.373283 },
+        { lat: 48.817219, lng: 2.373283 },
+        { lat: 48.818019, lng: 2.373283 }
+    ]
+
+    return (
+        <Map google={this.props.google}
+            style={style}
+            initialCenter={{
+              lat: 48.815619,
+              lng: 2.362983
+            }}
+            zoom={15}
+            onClick={this.onMapClicked}>
+          <Marker
+            onClick={this.onMarkerClick}
+            title={'Current location'}
+            name={'My location'}>
+
+          </Marker>
+          <Marker
+            onClick={this.onMarkerClick}
+            title={'Title as tooltip 1'}
+            name={'SOMA'}
+            position={points[0]}
+            onClick={this.onMarkerClick} />
+
+          <Marker
+            onClick={this.onMarkerClick}
+            title={'Title as tooltip 2'}
+            name={'Dolores park'}
+            position={points[1]}
+            onClick={this.onMarkerClick} />
+
+          <Marker onClick={this.onMarkerClick}
                 name={'Current location'} />
 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-      </Map>
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}>
+                <div>
+                  <h1>{this.state.selectedPlace.name}</h1>
+                </div>
+            </InfoWindow>
+        </Map>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: ('AIzaSyApEPJwn0VJNMbsnGzunmWgDxtNRCAwtnA')
+  apiKey: ('AIzaSyCZsidxj-jegaO0xMAFZkpOn7KsCk1a2rg')
 })(MapContainer)
