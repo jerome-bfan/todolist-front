cc0b2ff82705b3ffec6bfccb5de0017c29dea3e3
tos9lm <guillaume.rodier@epitech.eu>
2019-04-09 15:31:42 +0200

    Conditionnal form

--- P2PE/src/views/UserProfile/UserProfile.jsx
+++ P2PE/src/views/UserProfile/UserProfile.jsx
@@ -17,239 +17,242 @@ import avatar from "assets/img/faces/face-3.jpg";
 
 class UserProfile extends Component {
   _userProfilRender() {
-    return (
-      <div>
-        <Col md={8}>
-          <Card
-            title="Edit Profile"
-            content={
-              <form>
-                <FormInputs
-                  ncols={["col-md-5", "col-md-3", "col-md-4"]}
-                  proprieties={[
-                    {
-                      label: "First name",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "First name",
-                      defaultValue: "Mike"
-                    },
-                    {
-                      label: "Last name",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "Last name",
-                      defaultValue: "Andrew"
-                    },
-                    {
-                      label: "Email address",
-                      type: "email",
-                      bsClass: "form-control",
-                      placeholder: "Email",
-                      defaultValue: "mike.andrew@gmail.com"
-                    }
-                  ]}
-                />
-                <FormInputs
-                  ncols={["col-md-12"]}
-                  proprieties={[
-                    {
-                      label: "Phone number",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "Phone number",
-                      defaultValue: "0655423909"
-                    }
-                  ]}
-                />
-                <FormInputs
-                  ncols={["col-md-12"]}
-                  proprieties={[
-                    {
-                      label: "Adress",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "Home Adress",
-                      defaultValue:
-                        "Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                    }
-                  ]}
-                />
-                <FormInputs
-                  ncols={["col-md-4", "col-md-4", "col-md-4"]}
-                  proprieties={[
-                    {
-                      label: "City",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "City",
-                      defaultValue: "New York"
-                    },
-                    {
-                      label: "Country",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "Country",
-                      defaultValue: "USA"
-                    },
-                    {
-                      label: "Postal Code",
-                      type: "number",
-                      bsClass: "form-control",
-                      placeholder: "Postal Code",
-                      defaultValue: "10001"
-                    }
-                  ]}
-                />
+    var userform = false;
+    if (userform == true) {
+      userform = (
+        <div>
+          <Col md={8}>
+            <Card
+              title="Edit Profile"
+              content={
+                <form>
+                  <FormInputs
+                    ncols={["col-md-5", "col-md-3", "col-md-4"]}
+                    proprieties={[
+                      {
+                        label: "Company name",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "Company name",
+                        defaultValue: "Apple France"
+                      },
+                      {
+                        label: "SIRET",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "SIRET",
+                        defaultValue: "32212091600208"
+                      },
+                      {
+                        label: "Number of employees",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "Number of employees",
+                        defaultValue: "199"
+                      }
+                    ]}
+                  />
+                  <Row>
+                    <Col md={12}>
+                      <FormGroup controlId="formControlsTextarea">
+                        <ControlLabel>About the company</ControlLabel>
+                        <FormControl
+                          rows="5"
+                          componentClass="textarea"
+                          bsClass="form-control"
+                          placeholder="Here can be the description of your company"
+                          defaultValue="Apple is a US-based multinational company that designs and markets consumer electronics, personal computers, and computer software."
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <FormInputs
+                    ncols={["col-md-5", "col-md-3", "col-md-4"]}
+                    proprieties={[
+                      {
+                        label: "First name",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "First name",
+                        defaultValue: "Mike"
+                      },
+                      {
+                        label: "Last name",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "Last name",
+                        defaultValue: "Andrew"
+                      },
+                      {
+                        label: "Email address",
+                        type: "email",
+                        bsClass: "form-control",
+                        placeholder: "Email address",
+                        defaultValue: "mike.andrew@gmail.com"
+                      }
+                    ]}
+                  />
+                  <FormInputs
+                    ncols={["col-md-12"]}
+                    proprieties={[
+                      {
+                        label: "Phone number",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "Phone number",
+                        defaultValue: "0655423909"
+                      }
+                    ]}
+                  />
+                  <FormInputs
+                    ncols={["col-md-12"]}
+                    proprieties={[
+                      {
+                        label: "Adress",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "Home Adress",
+                        defaultValue:
+                          "Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                      }
+                    ]}
+                  />
+                  <FormInputs
+                    ncols={["col-md-4", "col-md-4", "col-md-4"]}
+                    proprieties={[
+                      {
+                        label: "City",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "City",
+                        defaultValue: "New York"
+                      },
+                      {
+                        label: "Country",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "Country",
+                        defaultValue: "USA"
+                      },
+                      {
+                        label: "Postal Code",
+                        type: "number",
+                        bsClass: "form-control",
+                        placeholder: "Postal Code",
+                        defaultValue: "10001"
+                      }
+                    ]}
+                  />
 
-                <Button bsStyle="info" pullRight fill type="submit">
-                  Update Profile
-                </Button>
-                <div className="clearfix" />
-              </form>
-            }
-          />
-        </Col>
-      </div>
-    );
-  }
+                  <Button bsStyle="info" pullRight fill type="submit">
+                    Update Profile
+                  </Button>
+                  <div className="clearfix" />
+                </form>
+              }
+            />
+          </Col>
+        </div>
+      );
+    } else {
+      userform = (
+        <div>
+          <Col md={8}>
+            <Card
+              title="Edit Profile"
+              content={
+                <form>
+                  <FormInputs
+                    ncols={["col-md-5", "col-md-3", "col-md-4"]}
+                    proprieties={[
+                      {
+                        label: "First name",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "First name",
+                        defaultValue: "Mike"
+                      },
+                      {
+                        label: "Last name",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "Last name",
+                        defaultValue: "Andrew"
+                      },
+                      {
+                        label: "Email address",
+                        type: "email",
+                        bsClass: "form-control",
+                        placeholder: "Email",
+                        defaultValue: "mike.andrew@gmail.com"
+                      }
+                    ]}
+                  />
+                  <FormInputs
+                    ncols={["col-md-12"]}
+                    proprieties={[
+                      {
+                        label: "Phone number",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "Phone number",
+                        defaultValue: "0655423909"
+                      }
+                    ]}
+                  />
+                  <FormInputs
+                    ncols={["col-md-12"]}
+                    proprieties={[
+                      {
+                        label: "Adress",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "Home Adress",
+                        defaultValue:
+                          "Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                      }
+                    ]}
+                  />
+                  <FormInputs
+                    ncols={["col-md-4", "col-md-4", "col-md-4"]}
+                    proprieties={[
+                      {
+                        label: "City",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "City",
+                        defaultValue: "New York"
+                      },
+                      {
+                        label: "Country",
+                        type: "text",
+                        bsClass: "form-control",
+                        placeholder: "Country",
+                        defaultValue: "USA"
+                      },
+                      {
+                        label: "Postal Code",
+                        type: "number",
+                        bsClass: "form-control",
+                        placeholder: "Postal Code",
+                        defaultValue: "10001"
+                      }
+                    ]}
+                  />
 
-  _proProfilRender() {
-    return (
-      <div>
-        <Col md={8}>
-          <Card
-            title="Edit Profile"
-            content={
-              <form>
-                <FormInputs
-                  ncols={["col-md-5", "col-md-3", "col-md-4"]}
-                  proprieties={[
-                    {
-                      label: "Company name",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "Company name",
-                      defaultValue: "Apple France"
-                    },
-                    {
-                      label: "SIRET",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "SIRET",
-                      defaultValue: "32212091600208"
-                    },
-                    {
-                      label: "Number of employees",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "Number of employees",
-                      defaultValue: "199"
-                    }
-                  ]}
-                />
-                <Row>
-                  <Col md={12}>
-                    <FormGroup controlId="formControlsTextarea">
-                      <ControlLabel>About the company</ControlLabel>
-                      <FormControl
-                        rows="5"
-                        componentClass="textarea"
-                        bsClass="form-control"
-                        placeholder="Here can be the description of your company"
-                        defaultValue="Apple is a US-based multinational company that designs and markets consumer electronics, personal computers, and computer software."
-                      />
-                    </FormGroup>
-                  </Col>
-                </Row>
-                <FormInputs
-                  ncols={["col-md-5", "col-md-3", "col-md-4"]}
-                  proprieties={[
-                    {
-                      label: "First name",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "First name",
-                      defaultValue: "Mike"
-                    },
-                    {
-                      label: "Last name",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "Last name",
-                      defaultValue: "Andrew"
-                    },
-                    {
-                      label: "Email address",
-                      type: "email",
-                      bsClass: "form-control",
-                      placeholder: "Email address",
-                      defaultValue: "mike.andrew@gmail.com"
-                    }
-                  ]}
-                />
-                <FormInputs
-                  ncols={["col-md-12"]}
-                  proprieties={[
-                    {
-                      label: "Phone number",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "Phone number",
-                      defaultValue: "0655423909"
-                    }
-                  ]}
-                />
-                <FormInputs
-                  ncols={["col-md-12"]}
-                  proprieties={[
-                    {
-                      label: "Adress",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "Home Adress",
-                      defaultValue:
-                        "Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                    }
-                  ]}
-                />
-                <FormInputs
-                  ncols={["col-md-4", "col-md-4", "col-md-4"]}
-                  proprieties={[
-                    {
-                      label: "City",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "City",
-                      defaultValue: "New York"
-                    },
-                    {
-                      label: "Country",
-                      type: "text",
-                      bsClass: "form-control",
-                      placeholder: "Country",
-                      defaultValue: "USA"
-                    },
-                    {
-                      label: "Postal Code",
-                      type: "number",
-                      bsClass: "form-control",
-                      placeholder: "Postal Code",
-                      defaultValue: "10001"
-                    }
-                  ]}
-                />
+                  <Button bsStyle="info" pullRight fill type="submit">
+                    Update Profile
+                  </Button>
+                  <div className="clearfix" />
+                </form>
+              }
+            />
+          </Col>
+        </div>
+      );
+    }
 
-                <Button bsStyle="info" pullRight fill type="submit">
-                  Update Profile
-                </Button>
-                <div className="clearfix" />
-              </form>
-            }
-          />
-        </Col>
-      </div>
-    );
+    return userform;
   }
 
   render() {
@@ -288,9 +291,6 @@ class UserProfile extends Component {
                 }
               />
             </Col>
-          </Row>
-          <Row>
-            {this._proProfilRender()}
           </Row>
         </Grid>
       </div>
