7128181165d01d56179ca24c9b12cf4d4f49979c
tos9lm <guillaume.rodier@epitech.eu>
2019-04-15 11:44:28 +0200

    DashboardCustomer outside dashboard

---
+++ P2PE/src/components/Dashboard/DashboardCustomer.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import Map from "components/Map/GGMap";
+import { Grid, Row, Col, Panel, PanelGroup } from "react-bootstrap";
+import { style } from "variables/Variables.jsx";
+import { Card } from "components/Card/Card.jsx";
+
+class DashboardCustomer extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <div>
+        <Col md={12} style={{ paddingLeft: "5px", paddingRight: "5px", height: "600px" }}>
+          <Map></Map>
+        </Col>
+        <Col md={12}>
+          <Card
+            content={
+              <div style={{ flexDirection: "column" }}>
+                <div className="">
+                  <div className="App-header">
+                    <h2 style={style.title}>Présentation de l'entreprise</h2>
+                  </div>
+                  <PanelGroup
+                    accordion
+                    id="accordion_problems"
+                    onSelect={this.handleSelect}
+                  >
+                    <Panel eventKey="1">
+                      <Panel.Heading>
+                        <Panel.Title toggle style={style.question}>
+                          Ce que nous faisons
+                        </Panel.Title>
+                      </Panel.Heading>
+                      <Panel.Body>
+                        Notre site vous propose de vous mettre en relation avec
+                        des pros. Ces derniers vous proposeront de multiples
+                        services, que ce soit pour des déménagements, du jardinage
+                        ou tout autre action du quotidien.
+                      </Panel.Body>
+                    </Panel>
+                  </PanelGroup>
+                </div>
+              </div>
+            }
+          />
+        </Col>
+      </div>
+    );
+  }
+}
+
+export default DashboardCustomer;
--- P2PE/src/views/Dashboard/Dashboard.jsx
+++ P2PE/src/views/Dashboard/Dashboard.jsx
@@ -14,7 +14,7 @@ import AWS from "aws-sdk/dist/aws-sdk-react-native";
 import { Card } from "components/Card/Card.jsx";
 import NotificationSystem from "react-notification-system";
 
-import Map from "components/Map/GGMap";
+import DashboardCustomer from "components/Dashboard/DashboardCustomer";
 
 import {
   authentification,
@@ -233,51 +233,6 @@ class Dashboard extends Component {
     }
   }
 
-  _rendrePresentation() {
-    return (
-      <div>
-        <Col md={12} style={{ paddingLeft: "5px", paddingRight: "5px", height: "600px" }}>
-          <Map></Map>
-        </Col>
-        <Col md={12}>
-          <Card
-            //title="Inscrivez-vous"
-            //category="Remplir les informations ci dessous"
-            content={
-              <div style={{ flexDirection: "column" }}>
-                <div className="">
-                  <div className="App-header">
-                    <h2 style={style.title}>Présentation de l'entreprise</h2>
-                  </div>
-                  <PanelGroup
-                    accordion
-                    id="accordion_problems"
-                    activeKey={this.state.activeKey}
-                    onSelect={this.handleSelect}
-                  >
-                    <Panel eventKey="1">
-                      <Panel.Heading>
-                        <Panel.Title toggle style={style.question}>
-                          Ce que nous faisons
-                        </Panel.Title>
-                      </Panel.Heading>
-                      <Panel.Body>
-                        Notre site vous propose de vous mettre en relation avec
-                        des pros. Ces derniers vous proposeront de multiples
-                        services, que ce soit pour des déménagements, du jardinage
-                        ou tout autre action du quotidien.
-                      </Panel.Body>
-                    </Panel>
-                  </PanelGroup>
-                </div>
-              </div>
-            }
-          />
-        </Col>
-      </div>
-    );
-  }
-
   _renderConnected() {
     if (this.state.connected) {
       this.props.handleConnected()
@@ -286,7 +241,7 @@ class Dashboard extends Component {
           <Grid fluid>
             <Row>
               <h2 style={style.title}>Bienvenue sur la page d'accueil</h2>
-              {this._rendrePresentation()}
+              <DashboardCustomer></DashboardCustomer>
             </Row>
           </Grid>
           {displayHomeService()}
