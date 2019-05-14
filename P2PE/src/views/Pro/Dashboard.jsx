f1f6e6664d245e5e5f89b95a93e8449d6ba4d183
tos9lm <guillaume.rodier@epitech.eu>
2019-05-14 11:41:03 +0200

    remove state service

--- P2PE/src/views/Pro/Dashboard.jsx
+++ P2PE/src/views/Pro/Dashboard.jsx
@@ -3,7 +3,6 @@ import { getServicePro, putValidateService } from "../../Provider/Api";
 import { Grid, Row, Col, Panel, PanelGroup } from "react-bootstrap";
 import { style } from "variables/Variables.jsx";
 import { Card } from "components/Card/Card.jsx";
-import { StateService } from "../ServicesUser/RequestServices";
 
 const myStyle = {
   title : {
