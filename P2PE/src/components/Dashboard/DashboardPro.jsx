f1b5d7661a31b25d890c40192ffea34850bac892
jerome2.jouanny <jerome2.jouanny@epitech.eu>
2019-09-22 11:19:43 +0200

    make the system with the code for a pro

--- P2PE/src/components/Dashboard/DashboardPro.jsx
+++ P2PE/src/components/Dashboard/DashboardPro.jsx
@@ -1,49 +1,61 @@
 import React, { Component } from "react";
-import { Grid, Row, Col, Panel, PanelGroup } from "react-bootstrap";
+import { FormInputs } from "components/FormInputs/FormInputs.jsx";
+
+import {
+  Grid,
+  Row,
+  Col,
+  Panel,
+  PanelGroup,
+  Modal,
+  FormControl,
+  FormGroup,
+  ControlLabel
+} from "react-bootstrap";
 import { style } from "variables/Variables.jsx";
 import { Card } from "components/Card/Card.jsx";
 import { StateService } from "../ServicesUser/RequestServicesCard";
 import { url, getHeaders, putHeader } from "../../Provider/Api";
-import Button from 'components/CustomButton/CustomButton';
+import Button from "components/CustomButton/CustomButton";
 
 const myStyle = {
-  title : {
-    textAlign: 'center',
-    margin: '20px 0 30px',
-    fontWeight: 'bold'
+  title: {
+    textAlign: "center",
+    margin: "20px 0 30px",
+    fontWeight: "bold"
   },
   line: {
-    height: '1px',
-    border: '0',
-    borderTop: '1px solid #c2c2c2',
-    margin: '2em 0',
-    padding: '0',
+    height: "1px",
+    border: "0",
+    borderTop: "1px solid #c2c2c2",
+    margin: "2em 0",
+    padding: "0"
   },
   ul: {
-    listStyleType: 'none',
-    margin: '0',
-    padding: '0',
-    overflow: 'hidden',
-    title : {
-      fontWeight: 'bold',
-      margin: '10px 0 5px'
+    listStyleType: "none",
+    margin: "0",
+    padding: "0",
+    overflow: "hidden",
+    title: {
+      fontWeight: "bold",
+      margin: "10px 0 5px"
     },
     text: {
-      fontSize: '20px'
+      fontSize: "20px"
     },
-    li : {
-      float: 'left',
-      display: 'block',
-      textAlign: 'center',
-      padding: '0 100px',
-      textDecoration: 'none'
+    li: {
+      float: "left",
+      display: "block",
+      textAlign: "center",
+      padding: "0 100px",
+      textDecoration: "none"
     },
-    liBis : {
-      float: 'right',
-      display: 'block',
-      textAlign: 'center',
-      padding: '0 100px',
-      textDecoration: 'none'
+    liBis: {
+      float: "right",
+      display: "block",
+      textAlign: "center",
+      padding: "0 100px",
+      textDecoration: "none"
     }
   }
 };
@@ -52,8 +64,13 @@ class DashboardPro extends Component {
   constructor(props, context) {
     super(props, context);
     this.acceptRequest = this.acceptRequest.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this._renderModalUpdate = this._renderModalUpdate.bind(this);
 
     this.state = {
+      modalShow: false,
+      codeMessage: "",
       /*services: {
         service1 : {
           title: 'Jardinage',
@@ -83,43 +100,51 @@ class DashboardPro extends Component {
           validate: false
         }
       },*/
-      services : [],
+      services: [],
       notification: 0
     };
   }
 
   componentDidMount() {
-    fetch(url + "pro/"+ localStorage.pro_id +"/requested_services/extend", getHeaders())
-    .then(res => res.json()).then(
-      (result) => {
-        const requested_services = result.map(function(requested_service) {
-          return {
-            s_id: requested_service.proposed_id,
-            s_id_pro: requested_service.proposed_id_pro,
-            s_name: requested_service.proposed_name,
-            s_description: requested_service.proposed_description,
-            s_price: requested_service.proposed_price,
-            s_options: requested_service.proposed_option,
-            r_id: requested_service.requested_id,
-            r_id_user: requested_service.requested_id_user,
-            r_address: requested_service.requested_address,
-            r_date: new Date(requested_service.requested_creation_date),
-            r_date_expiration: new Date(requested_service.requested_expiration_date),
-            r_state: requested_service.requested_state,
-            r_paid: requested_service.requested_paid,
-          };
-        })
-        this.setState({
-          services: requested_services
-        })
-      },
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error: "ERROR"
-        });
-      }
+    fetch(
+      url + "pro/" + localStorage.pro_id + "/requested_services/extend",
+      getHeaders()
     )
+      .then(res => res.json())
+      .then(
+        result => {
+          console.log(result);
+          const requested_services = result.map(function(requested_service) {
+            return {
+              s_id: requested_service.proposed_id,
+              s_id_pro: requested_service.proposed_id_pro,
+              s_name: requested_service.proposed_name,
+              s_description: requested_service.proposed_description,
+              s_price: requested_service.proposed_price,
+              s_options: requested_service.proposed_option,
+              r_id: requested_service.requested_id,
+              r_id_user: requested_service.requested_id_user,
+              r_address: requested_service.requested_address,
+              r_code: requested_service.requested_code,
+              r_date: new Date(requested_service.requested_creation_date),
+              r_date_expiration: new Date(
+                requested_service.requested_expiration_date
+              ),
+              r_state: requested_service.requested_state,
+              r_paid: requested_service.requested_paid
+            };
+          });
+          this.setState({
+            services: requested_services
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error: "ERROR"
+          });
+        }
+      );
   }
 
   todayString() {
@@ -141,77 +166,86 @@ class DashboardPro extends Component {
     return (
       <div>
         <Col md={12}>
-          <h2 style={myStyle.title}>
-            Demandes en attente
-          </h2>
-          {Object
-            .keys(this.state.services)
-            .map(key =>
-              this.state.services[key].r_state === "Pending" ?
-                <Card
-                  key = {key}
-                  content={
+          <h2 style={myStyle.title}>Demandes en attente</h2>
+          {Object.keys(this.state.services).map(key =>
+            this.state.services[key].r_state === "Pending" ? (
+              <Card
+                key={key}
+                content={
+                  <div>
                     <div>
-                      <div>
-                        <ul style={myStyle.ul}>
-                          <li style={myStyle.ul.li}>
-                            <h2 style={myStyle.ul.title}>
-                              { this.state.services[key].s_name }
-                            </h2>
-                          </li>
-                          <li style={myStyle.ul.liBis}>
-                            <h2 style={myStyle.ul.title}>
-                              { this.state.services[key].s_price + "€" }
-                            </h2>
-                          </li>
-                        </ul>
-                      </div>
-                      <div style={myStyle.line}></div>
-                      <div>
-                        <ul style={myStyle.ul}>
-                          <li style={myStyle.ul.li}>
-                            <div style={myStyle.ul.text}>
-                              { this.state.services[key].s_description }
-                            </div>
-                          </li>
-                        </ul>
-                      </div>
-                      <div>
-                        <ul style={myStyle.ul}>
-                          <li style={myStyle.ul.li}>
-                            <div style={myStyle.ul.text}>
-                                <b>Demande au : </b>
-                                { this.state.services[key].r_address }
-                              </div>
-                          </li>
-                          <li style={myStyle.ul.liBis}>
-                            <div style={myStyle.ul.text}>
-                              <b>Le : </b>
-                              { this.state.services[key].r_date.toLocaleDateString("fr-FR") }
-                            </div>
-                          </li>
-                        </ul>
-                      </div>
-                      <div style={{
+                      <ul style={myStyle.ul}>
+                        <li style={myStyle.ul.li}>
+                          <h2 style={myStyle.ul.title}>
+                            {this.state.services[key].s_name}
+                          </h2>
+                        </li>
+                        <li style={myStyle.ul.liBis}>
+                          <h2 style={myStyle.ul.title}>
+                            {this.state.services[key].s_price + "€"}
+                          </h2>
+                        </li>
+                      </ul>
+                    </div>
+                    <div style={myStyle.line}></div>
+                    <div>
+                      <ul style={myStyle.ul}>
+                        <li style={myStyle.ul.li}>
+                          <div style={myStyle.ul.text}>
+                            {this.state.services[key].s_description}
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                    <div>
+                      <ul style={myStyle.ul}>
+                        <li style={myStyle.ul.li}>
+                          <div style={myStyle.ul.text}>
+                            <b>Demande au : </b>
+                            {this.state.services[key].r_address}
+                          </div>
+                        </li>
+                        <li style={myStyle.ul.liBis}>
+                          <div style={myStyle.ul.text}>
+                            <b>Le : </b>
+                            {this.state.services[key].r_date.toLocaleDateString(
+                              "fr-FR"
+                            )}
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                    <div
+                      style={{
                         float: "right",
                         marginRight: 90
-                      }}>  
-                        <Button bsStyle="success"
-                          onClick={e=>{
-                            this.acceptRequest(key)
-                          }}
-                        >Accepter</Button>
-                        <b> </b>
-                        <Button bsStyle="danger"
-                          onClick={e=>{
-                            this.refuseRequest(key)
-                          }}
-                        >Refuser</Button>
-                      </div>
+                      }}
+                    >
+                      {this._renderModalUpdate(key)}
+                      <Button
+                        bsStyle="success"
+                        onClick={e => {
+                          this.setState({
+                            modalShow: true
+                          });
+                        }}
+                      >
+                        Accepter
+                      </Button>
+                      <b> </b>
+                      <Button
+                        bsStyle="danger"
+                        onClick={e => {
+                          this.refuseRequest(key);
+                        }}
+                      >
+                        Refuser
+                      </Button>
                     </div>
-                  }
-                />
-              : null
+                  </div>
+                }
+              />
+            ) : null
           )}
         </Col>
       </div>
@@ -220,29 +254,34 @@ class DashboardPro extends Component {
 
   acceptRequest(key) {
     this.setState({
-      isLoaded:true
-    })
-    fetch(url + "requested_services/" + this.state.services[key].r_id + "/state", putHeader(
-      "{\"state\":\"Accepted\"}"
-    )).then(result => {
+      modalShow: true
+    });
+    this.setState({
+      isLoaded: true
+    });
+    fetch(
+      url + "requested_services/" + this.state.services[key].r_id + "/state",
+      putHeader('{"state":"Accepted"}')
+    ).then(result => {
       console.log(result);
       this.state.services[key].r_state = "Accepted";
       this.setState({
-        isLoaded:true
-      })
-    })
+        isLoaded: true
+      });
+    });
   }
-  
+
   refuseRequest(key) {
-    fetch(url + "requested_services/" + this.state.services[key].r_id + "/state", putHeader(
-      "{\"state\":\"Refused\"}"
-    )).then(result => {
+    fetch(
+      url + "requested_services/" + this.state.services[key].r_id + "/state",
+      putHeader('{"state":"Refused"}')
+    ).then(result => {
       console.log(result);
       this.state.services[key].r_state = "Refused";
       this.setState({
-        isLoaded:true
-      })
-    })
+        isLoaded: true
+      });
+    });
   }
 
   render_services_prochain() {
@@ -251,67 +290,124 @@ class DashboardPro extends Component {
     return (
       <div>
         <Col md={12}>
-          <h2 style={myStyle.title}>
-            Services à venir
-          </h2>
-          {Object
-            .keys(this.state.services)
-            .map(key =>
-              this.state.services[key].r_state === "Accepted" ?
-                <Card
-                  key = {key}
-                  content={
+          <h2 style={myStyle.title}>Services à venir</h2>
+          {Object.keys(this.state.services).map(key =>
+            this.state.services[key].r_state === "Accepted" ? (
+              <Card
+                key={key}
+                content={
+                  <div>
                     <div>
-                      <div>
-                        <ul style={myStyle.ul}>
-                          <li style={myStyle.ul.li}>
-                            <h2 style={myStyle.ul.title}>
-                              { this.state.services[key].s_name }
-                            </h2>
-                          </li>
-                          <li style={myStyle.ul.liBis}>
-                            <h2 style={myStyle.ul.title}>
-                              { this.state.services[key].s_price + "€" }
-                            </h2>
-                          </li>
-                        </ul>
-                      </div>
-                      <div style={myStyle.line}></div>
-                      <div>
-                        <ul style={myStyle.ul}>
-                          <li style={myStyle.ul.li}>
-                            <div style={myStyle.ul.text}>
-                              { this.state.services[key].s_description }
-                            </div>
-                          </li>
-                        </ul>
-                      </div>
-                      <div>
-                        <ul style={myStyle.ul}>
-                          <li style={myStyle.ul.li}>
-                            <div style={myStyle.ul.text}>
-                                <b>Demande au : </b>
-                                { this.state.services[key].r_address }
-                              </div>
-                          </li>
-                          <li style={myStyle.ul.liBis}>
-                            <div style={myStyle.ul.text}>
-                              <b>Le : </b>
-                              { this.state.services[key].r_date.toLocaleDateString("fr-FR") }
-                            </div>
-                          </li>
-                        </ul>
-                      </div>
+                      <ul style={myStyle.ul}>
+                        <li style={myStyle.ul.li}>
+                          <h2 style={myStyle.ul.title}>
+                            {this.state.services[key].s_name}
+                          </h2>
+                        </li>
+                        <li style={myStyle.ul.liBis}>
+                          <h2 style={myStyle.ul.title}>
+                            {this.state.services[key].s_price + "€"}
+                          </h2>
+                        </li>
+                      </ul>
                     </div>
-                  }
-                />
-              : null
+                    <div style={myStyle.line}></div>
+                    <div>
+                      <ul style={myStyle.ul}>
+                        <li style={myStyle.ul.li}>
+                          <div style={myStyle.ul.text}>
+                            {this.state.services[key].s_description}
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                    <div>
+                      <ul style={myStyle.ul}>
+                        <li style={myStyle.ul.li}>
+                          <div style={myStyle.ul.text}>
+                            <b>Demande au : </b>
+                            {this.state.services[key].r_address}
+                          </div>
+                        </li>
+                        <li style={myStyle.ul.liBis}>
+                          <div style={myStyle.ul.text}>
+                            <b>Le : </b>
+                            {this.state.services[key].r_date.toLocaleDateString(
+                              "fr-FR"
+                            )}
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                }
+              />
+            ) : null
           )}
         </Col>
       </div>
     );
   }
-
+  handleClose() {
+    this.setState({ modalShow: false });
+  }
+  handleSave() {}
+  _renderModalUpdate(key) {
+    console.log("Modal");
+    console.log(this.state.services[key]);
+    console.log("Modal");
+    return (
+      <div>
+        <Modal show={this.state.modalShow} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              Rentrez le code à 4 chiffres ! ({this.state.services[key].r_code}){" "}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {" "}
+            <FormInputs
+              ncols={["col-md-12"]}
+              proprieties={[
+                {
+                  label: "Code :",
+                  type: "text",
+                  id: "code",
+                  defaultValue: "",
+                  bsClass: "form-control",
+                  placeholder: "Saisir le code ici",
+                  inputRef: input => (this.inputCode = input)
+                }
+              ]}
+            />
+            {this.state.codeMessage}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="primary"
+              onClick={e => {
+                console.log(this.inputCode.value);
+                console.log(this.state.services[key].r_code);
+                if (this.inputCode.value == this.state.services[key].r_code) {
+                  this.acceptRequest(key);
+                  this.setState({
+                    modalShow: false
+                  });
+                } else {
+                  this.setState({
+                    codeMessage:
+                      "Le code n'est pas bon veuillez verifier vos emails"
+                  });
+                }
+              }}
+            >
+              Valider
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
   render() {
     return (
       <div>
